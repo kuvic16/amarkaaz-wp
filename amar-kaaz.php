1143fdc2250c6c5d1034a5b68bb8e9f8ef0c9b63
Shaiful Islam <kuvic16@gmail.com>
2020-05-14 02:44:01 +0600

    AmarKaaz wordpress plugin development started after watching (Kamywaab) movie

---
+++ amar-kaaz.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Plugin Name: Amar Kaaz
+ * Description: A new way to manage personal task aiming to keep focusing
+ * Plugin URL: https://amarkaaz.com
+ * Author: Shaiful Islam Palash
+ * Author URL: https://sheemoul.wordpress.com
+ * Version: 1.0
+ * License: GPLv2 in
+ * License URL: https://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+if(!defined('ABSPATH')){
+    exit;
+}
