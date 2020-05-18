591a6e50cd1a789062bc412ae13f93aabda2dc5e
Shaiful Islam <kuvic16@gmail.com>
2020-05-18 23:53:07 +0600

    Including assets: In progress

---
+++ src/Assets.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Amar\Kaaz;
+
+/**
+ * Assets handlers class
+ */
+class Assets {
+
+    /**
+     * Initialize the class
+     */
+    function __construct() {
+        add_action('wp_enqueue_scripts', [$this, 'enqueue_assets']);
+        add_action('admin_enqueue_scripts', [$this, 'enqueue_assets']);
+    }
+
+    public function enqueue_assets() {
+        
+    }
+}
