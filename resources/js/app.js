278c73cd8d34566a7be84783bcff2f358ee2f8c4
Shaiful Islam <kuvic16@gmail.com>
2020-05-25 21:52:13 +0600

    webpack and tailwind included for frontend development

---
+++ resources/js/app.js
@@ -0,0 +1,13 @@
+import Vue from "Vue";
+import VueRouter from "vue-router";
+// import routes from "./routes";
+import axios from "axios";
+
+window.axios = axios;
+window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
+
+Vue.use(VueRouter);
+
+let app = new Vue({
+  el: "#app",
+});
