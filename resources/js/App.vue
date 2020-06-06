0b2958c43ffab4e552b2eb478c51442f93e5b4b6
Shaiful Islam <kuvic16@gmail.com>
2020-06-07 01:26:15 +0600

    menu name: in progress

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,56 +1,56 @@
 <template>
   <div id="vue-backend-app">
-    <div class="container px-8 pb-10">
+    <div class="px-8 pb-10">
       <main class="flex">
         <aside class="w-64 pt-8">
           <section class="mb-10">
             <h5 class="uppercase font-bold mb-5 text-base">Amar Kaaz</h5>
             <ul class="list-reset">
               <li class="text-sm leading-loose">
-                <router-link class="text-black" to="/" exact>Logo</router-link>
+                <router-link class="text-black" to="/" exact>Home</router-link>
               </li>
               <li class="text-sm leading-loose">
-                <router-link class="text-black" to="/logo-symbol">Logo Symbol</router-link>
+                <router-link class="text-black" to="/logo-symbol">Today</router-link>
               </li>
               <li class="text-sm leading-loose">
-                <router-link class="text-black" to="/colors">Colors</router-link>
+                <router-link class="text-black" to="/colors">Previous</router-link>
               </li>
               <li class="text-sm leading-loose">
-                <router-link class="text-black" to="/typography">Typography</router-link>
+                <router-link class="text-black" to="/typography">Upcoming</router-link>
+              </li>
+              <li class="text-sm leading-loose">
+                <router-link class="text-black" to="/typography">Repeat</router-link>
               </li>
             </ul>
           </section>
           <section class="mb-10">
-            <h5 class="uppercase font-bold mb-5 text-base">Doodle</h5>
+            <h5 class="uppercase font-bold mb-5 text-base">Progress</h5>
             <ul class="list-reset">
               <li class="text-sm leading-loose">
-                <router-link class="text-black" to="/mascot">Mascot</router-link>
+                <router-link class="text-black" to="/mascot">Calender Graph</router-link>
               </li>
               <li class="text-sm leading-loose">
-                <router-link class="text-black" to="/illustration">Illustration</router-link>
-              </li>
-              <li class="text-sm leading-loose">
-                <router-link class="text-black" to="/loaders-animation">Loaders and Animation</router-link>
-              </li>
-              <li class="text-sm leading-loose">
-                <router-link class="text-black" to="/wallpapers">Wallpapers</router-link>
+                <router-link class="text-black" to="/illustration">Insight</router-link>
               </li>
             </ul>
           </section>
           <section>
-            <h5 class="uppercase font-bold mb-5 text-base">Stats</h5>
+            <h5 class="uppercase font-bold mb-5 text-base">Settings</h5>
             <ul class="list-reset">
               <li class="text-sm leading-loose">
-                <router-link class="text-black" to="/site-stats">Site Stats</router-link>
+                <router-link class="text-black" to="/site-stats">Kaaz Category</router-link>
               </li>
               <li class="text-sm leading-loose">
-                <router-link class="text-black" to="/achievements">Achievements</router-link>
+                <router-link class="text-black" to="/achievements">Notifications</router-link>
               </li>
             </ul>
           </section>
         </aside>
-        <div class="primary flex-1">
-          <router-view></router-view>
+        <div class="primary flex-1 pt-8">
+          <div>
+            <span class="float-right">A new way to manage personal task aiming to keep focusing</span>
+          </div>
+          <router-view class="mt-10"></router-view>
         </div>
       </main>
     </div>
