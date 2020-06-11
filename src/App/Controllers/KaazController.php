662ae6300d7c3c584f80896869bf23150d1b0e1a
Shaiful Islam <kuvic16@gmail.com>
2020-06-11 23:21:33 +0600

    KaazController added to serve kaaz realated api

---
+++ src/App/Controllers/KaazController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Amar\Kaaz\App\Controllers;
+
+/**
+ * Kaaz controller class
+ */
+class KaazController
+{
+
+    /**
+     * Get kaaz list
+     */
+    public function list()
+    {
+    }
+
+    /**
+     * Create new kaaz
+     */
+    public function create()
+    {
+    }
+
+    /**
+     * Update a kaaz
+     */
+    public function update()
+    {
+    }
+
+    /**
+     * Delete a kaaz
+     */
+    public function delete()
+    {
+    }
+}
