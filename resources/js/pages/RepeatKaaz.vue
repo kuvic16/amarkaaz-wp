68bd4c895d1e1f9fd9077117caafb5cc873512a1
Shaiful Islam <kuvic16@gmail.com>
2020-06-15 23:38:50 +0600

    Repeat kaaz layout in progress

--- resources/js/pages/RepeatKaaz.vue
+++ resources/js/pages/RepeatKaaz.vue
@@ -7,44 +7,31 @@
         <div class="w-full px-3">
           <label
             class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-            for="grid-first-name"
+            for="grid-task-name"
           >Task Name</label>
           <input
             class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-            id="grid-first-name"
+            id="grid-task-name"
             type="text"
-            placeholder="Jane"
+            placeholder="Ex. Exercise"
           />
           <p class="text-red-500 text-xs italic">Please fill out this field.</p>
-        </div>
-      </div>
-      <div class="flex flex-wrap -mx-3 mb-6">
-        <div class="w-full px-3">
-          <label
-            class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-            for="grid-password"
-          >Password</label>
-          <input
-            class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-            id="grid-password"
-            type="password"
-            placeholder="******************"
-          />
-          <p class="text-gray-600 text-xs italic">Make it as long and as crazy as you'd like</p>
         </div>
       </div>
       <div class="flex flex-wrap -mx-3 mb-2">
         <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
           <label
             class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-            for="grid-city"
-          >City</label>
-          <input
-            class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-            id="grid-city"
-            type="text"
-            placeholder="Albuquerque"
-          />
+            for="grid-task-type"
+          >Type</label>
+          <span>
+            <input
+              class="block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+              id="grid-city"
+              type="radio"
+            />
+            Nice to have
+          </span>
         </div>
         <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
           <label
