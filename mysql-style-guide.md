6d8e9d6dfc09ade5a4544862b473f6cdc5021c2f
Shaiful Islam <kuvic16@gmail.com>
2020-06-16 10:46:09 +0600

    repeat kaaz layout designing - in progress

---
+++ mysql-style-guide.md
@@ -0,0 +1,12 @@
+# SQL Style Guide
+
+SQL Style guide to design standard database and query by Simon Holywell. https://www.sqlstyle.guide/. https://raw.githubusercontent.com/treffynnon/sqlstyle.guide/gh-pages/_includes/sqlstyle.guide.md
+
+## General
+
+### Do
+
+- Use consistent and descriptive identifiers and names
+- Make judicious use of white space and indentation to make code easier to read.
+- Store [ISO 8601][iso-8601] compliant time and date information (`YYYY-MM-DD HH:MM:SS.SSSSS`).
+- Try to only use standard SQL funtions instead of vendor-specific functions for reasons of portability
--- resources/js/pages/RepeatKaaz.vue
+++ resources/js/pages/RepeatKaaz.vue
@@ -2,7 +2,7 @@
   <div>
     <h1 class="font-normal text-3xl text-gray-700 leading-none">Repeat Kaaz</h1>
 
-    <form class="w-full max-w-lg mt-10">
+    <form class="w-1/3 max-w-lg mt-10">
       <div class="flex flex-wrap -mx-3 mb-6">
         <div class="w-full px-3">
           <label
@@ -18,61 +18,38 @@
           <p class="text-red-500 text-xs italic">Please fill out this field.</p>
         </div>
       </div>
-      <div class="flex flex-wrap -mx-3 mb-2">
-        <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+      <div class="flex flex-wrap -mx-3 mb-6">
+        <div class="w-full px-3">
           <label
             class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
             for="grid-task-type"
           >Type</label>
-          <span>
-            <input
-              class="block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-              id="grid-city"
-              type="radio"
-            />
+          <label>
+            <input class id="grid-city" type="radio" name="task-type" />
             Nice to have
-          </span>
+          </label>
+          <label class="ml-2">
+            <input class id="grid-city" type="radio" name="task-type" />
+            Must to have
+          </label>
         </div>
-        <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+      </div>
+      <div class="flex flex-wrap -mx-3 mb-6">
+        <div class="w-full px-3">
           <label
             class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
             for="grid-state"
-          >State</label>
+          >Repeat Policy</label>
           <div class="relative">
             <select
-              class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+              class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
               id="grid-state"
             >
-              <option>New Mexico</option>
-              <option>Missouri</option>
-              <option>Texas</option>
+              <option>Daily</option>
+              <option>Monthly</option>
+              <option>Yearly</option>
             </select>
-            <div
-              class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700"
-            >
-              <svg
-                class="fill-current h-4 w-4"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-              >
-                <path
-                  d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"
-                />
-              </svg>
-            </div>
           </div>
-        </div>
-        <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-          <label
-            class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-            for="grid-zip"
-          >Zip</label>
-          <input
-            class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-            id="grid-zip"
-            type="text"
-            placeholder="90210"
-          />
         </div>
       </div>
     </form>
