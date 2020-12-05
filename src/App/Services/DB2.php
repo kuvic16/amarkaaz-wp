ffb3ee4324017a86d651ec34c659f762682d7019
Shaiful Islam Palash <kuvic16@gmail.com>
2020-12-05 10:04:10 +0600

    db service 2: edit in progress

--- src/App/Services/DB2.php
+++ src/App/Services/DB2.php
@@ -484,13 +484,6 @@ class DB2
     {
         global $wpdb;
 
-        if (empty($args['name'])) {
-            return new \WP_Error(
-                'no-name',
-                __('You must provide a name', 'plugin-dev')
-            );
-        }
-
         $defaults = [
             'created_by' => get_current_user_id(),
             'created_at' => current_time('mysql')
@@ -515,25 +508,6 @@ class DB2
                 ['%d']
             );
             return $updated;
-        } else {
-            $inserted = $wpdb->insert(
-                "{$wpdb->prefix}pd_addresses",
-                $data,
-                [
-                    '%s',
-                    '%s',
-                    '%s',
-                    '%d',
-                    '%s'
-                ]
-            );
-
-            if (!$inserted) {
-                return new \WP_Error(
-                    'failed-to-insert',
-                    __('Failed to insert data', 'plugin-dev')
-                );
-            }
         }
         return $wpdb->insert_id;
     }
