fa9e4ecbd3caa188dcd96a3d7a95bf92e71c91fa
Shaiful Islam Palash <kuvic16@gmail.com>
2020-12-24 23:58:38 +0600

    AM-2: edit repeat kaaz backend in progress

--- src/App/Services/RepeatKaazService.php
+++ src/App/Services/RepeatKaazService.php
@@ -90,60 +90,38 @@ class RepeatKaazService extends AbstractService
     }
 
     /**
-     * Create or Update the records
+     * Update the repeat_kaazs
      * 
      * @param array $args
      * 
      * @return int|WP_Error
      */
-    function save($args = [])
+    function update($args = [])
     {
-        global $wpdb;
+        $exist = DB2::table(self::$table_name)
+                ->where(['name' => $args['name', 'id' ]])->first();
+        
+        if($exist) {
+            throw new Exception(__('Already exist!', 'amar-kaaz'));
+        }
 
         $defaults = [
             'created_by' => get_current_user_id(),
-            'created_at' => current_time('mysql')
+            'created_at' => current_time('mysql'),
+            'updated_by' => get_current_user_id(),
+            'updated_at' => current_time('mysql')
         ];
-
 
         $data = wp_parse_args($args, $defaults);
         if (isset($data['id'])) {
-            $id = $data['id'];
             unset($data['id']);
-            $updated = $wpdb->update(
-                $wpdb->prefix . self::$table,
-                $data,
-                ['id' => $id],
-                [
-                    '%s',
-                    '%s',
-                    '%s',
-                    '%d',
-                    '%s'
-                ],
-                ['%d']
-            );
-            return $updated;
-        } else {
-            $inserted = $wpdb->insert(
-                $wpdb->prefix . self::$table,
-                $data,
-                [
-                    '%s',
-                    '%s',
-                    '%s',
-                    '%d',
-                    '%s'
-                ]
-            );
-
-            if (!$inserted) {
-                return new \WP_Error(
-                    'failed-to-insert',
-                    __('Failed to insert data', 'amar-kaaz')
-                );
-            }
         }
-        return $wpdb->insert_id;
+
+        $id = DB::table(self::$table_name)->create($data);        
+
+        if ($id === null) {
+            throw new Exception(__('Failed to insert data!', 'amar-kaaz'));            
+        }
+        return $id;
     }
 }
