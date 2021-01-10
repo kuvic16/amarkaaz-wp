75062d2cc665d76794bb96990f2a806d9cb1875c
Shaiful Islam Palash <kuvic16@gmail.com>
2021-01-10 20:56:04 +0600

    AM-4: service for kaaz

---
+++ src/App/Services/KaazService.php
@@ -0,0 +1,132 @@
+<?php
+
+namespace Amar\Kaaz\App\Services;
+
+use Amar\Kaaz\App\Constants\ITables;
+use Amar\Kaaz\App\Controllers\AlreadyExistException;
+use Amar\Kaaz\App\Controllers\ServiceException;
+use Exception;
+
+/**
+ * Repeat kaaz service
+ */
+class KaazService extends AbstractService
+{
+    /**
+     * Service class constructor
+     */
+    public function __construct()
+    {
+        parent::__construct(ITables::$KAAZS);
+    }
+
+     /**
+     * Get all the records of repeat kaaz
+     * 
+     * @return array|null
+     */
+    public function get_all()
+    {
+        return DB2::table(self::$table_name, 'rk')
+                ->inner_join(ITables::$KAAZ_TYPES, 'kt', 'kt.id = rk.kaaz_type_id')
+                ->select(['rk.*', 'kt.name as kaaz_type_name'])
+                ->order_by('rk.created_at desc')
+                ->get();
+    }
+
+    /**
+     * Get the records based on parameters
+     * 
+     * @param int $page - default 1
+     * @param int $limit - default 10
+     * 
+     * @return array|null
+     */
+    public function get_by($page = 1, $limit = 10)
+    {
+        return DB2::table(self::$table_name, 'rk')
+                ->inner_join(ITables::$KAAZ_TYPES, 'kt', 'kt.id = rk.kaaz_type_id')
+                ->select(['rk.*', 'kt.name as kaaz_type_name'])
+                ->order_by('rk.created_at desc')
+                ->page($page)
+                ->limit($limit)
+                ->pagination();
+    }
+    
+    /**
+     * Create the repeat_kaazs
+     * 
+     * @param array $args
+     * 
+     * @return int|WP_Error
+     */
+    function create($args = [])
+    {
+        $exist = DB::table(self::$table_name)
+                ->where(['name' => $args['name']])->first();
+        
+        if($exist) {
+            throw new Exception(__('Already exist!', 'amar-kaaz'));
+        }
+
+        $defaults = [
+            'created_by' => get_current_user_id(),
+            'created_at' => current_time('mysql')
+        ];
+
+        $data = wp_parse_args($args, $defaults);
+        if (isset($data['id'])) {
+            unset($data['id']);
+        }
+
+        $id = DB::table(self::$table_name)->create($data);        
+
+        if ($id === null) {
+            throw new Exception(__('Failed to insert data!', 'amar-kaaz'));            
+        }
+        return $id;
+    }
+
+    /**
+     * Update the repeat_kaazs
+     * 
+     * @param array $args
+     * 
+     * @return int|WP_Error
+     */
+    function update($args = [])
+    {
+        $exist = DB2::table(self::$table_name, 'rk')
+                ->where(['name' => $args['name']])
+                ->where(['id', '!=', $args['id']])
+                ->first();
+        
+        if($exist) {
+            throw new Exception(__('Already exist!', 'amar-kaaz'));
+        }
+
+        $defaults = [
+            'updated_by' => get_current_user_id(),
+            'updated_at' => current_time('mysql')
+        ];
+        $data = wp_parse_args($args, $defaults);
+
+        DB2::table(self::$table_name)->save($data);
+    }
+
+    /**
+     * delete the repeat_kaazs
+     * 
+     * @param integer $id
+     * 
+     * @return int|WP_Error
+     */
+    function delete($id)
+    {
+        $exist = DB2::table(self::$table_name)->find_by_id($id);
+        if(!$exist) {
+            throw new Exception(__('Not found!', 'amar-kaaz'));
+        }
+        DB2::table(self::$table_name)->delete($id);
+    }
+}
