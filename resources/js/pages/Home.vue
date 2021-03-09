c1f3db305adbdf69cff0cdaa4d387f33105aa665
Shaiful Islam Palash <kuvic16@gmail.com>
2021-03-09 23:56:32 +0600

    minor changes made

--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -3,6 +3,7 @@
     <h1 class="font-normal text-3xl text-gray-700 leading-none mb-8">
       Your current work
     </h1>
+    <p>This is page for showing the current task</p>
   </div>
 </template>
 
